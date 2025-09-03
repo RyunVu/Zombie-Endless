8e0ad6e1332b9bd2168353af8dcdfbe5c04c9b03
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2025-09-03 14:40:15 +0700

    Create Base movement Details

---
+++ Assets/_Scripts/Movement/MovememtDetailsSO.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MovementDetails_", menuName = "Scriptable Object/Movement/MovementDetails")]
+public class MovememtDetailsSO : ScriptableObject
+{
+    #region Header MOVEMENT DETAILS
+    [Space(10)]
+    [Header("MOVEMENT DETAILS")]
+    #endregion Header
+    #region Tooltip
+    [Tooltip("The minimum move speed. The GetMoveSpeed method calculates a random value between the minimum and maximum")]
+    #endregion Tooltip
+    public float minMoveSpeed = 8f;
+
+    #region Tooltip
+    [Tooltip("The maximum move speed. The GetMoveSpeed method calculates a random value between the minimum and maximum")]
+    #endregion Tooltip
+    public float maxMoveSpeed = 8f;
+
+    public float GetMoveSpeed()
+    {
+        if (minMoveSpeed == maxMoveSpeed) return minMoveSpeed;
+        else return Random.Range(minMoveSpeed, maxMoveSpeed);
+    }
+
+}
